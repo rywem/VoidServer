138bf8512e5ff5862bdc7b4ec2d2361fab36ace5
Ryan <cybervoid41@gmail.com>
2018-08-04 09:13:01 -0700

    added ..tests\Program.cs runner for running container and testing

--- VoidServerTests/Program.cs
+++ VoidServerTests/Program.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using Autofac;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using VoidServerLibrary;
+using VoidServerLibrary.Interfaces;
+using VoidServerLibrary.Listeners;
 
 namespace VoidServerTests
 {
     class Program
     {
+        static string[] http = new string[] { "http://127.0.0.1:8080/" };
+        static string[] socket = new string[] { "127.0.0.1" };
+        static bool isCanceled = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ServerManager.Start<VHttpListener>(http);
+            Console.WriteLine("Hit [ESC] to exit.");
+            while(isCanceled == false)
+            {
+                System.Threading.Thread.Sleep(100);
+                if(Console.KeyAvailable == true)
+                {
+                    if (Console.ReadKey().Key == ConsoleKey.Escape)
+                    {
+                        isCanceled = true;
+                        Console.WriteLine("Cancellation detecting, shutting server down.");
+                        ServerManager.Stop();
+                    }
+
+                }
+            }
+            Console.WriteLine("Stopping in 5 seconds.");
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+
+    public class ServerManager
+    {
+        
+        private static Task ServerTask { get; set; }
+        private static CancellationTokenSource _cancellationSource = new CancellationTokenSource();
+        public static void Start<T>(string[] args) where T : VoidServerLibrary.Interfaces.IListener
+        {
+            ServerTask = Task.Factory.StartNew(() => Runner<T>.Start(args), _cancellationSource.Token);
+        }
+
+        public static void Stop()
+        {
+            _cancellationSource.Cancel();
+
+            //while (ServerTask.IsCancell == false)
+            //    Thread.Sleep(100);
+
+            //ServerTask.Wait();
+        }
+    }
+    public class Runner<T> where T : IListener
+    {
+        public static void Start(string[] args)
+        {
+            var builder = new ContainerBuilder();            
+            builder.RegisterType<T>().As<IListener>();
+            builder.RegisterType<Server>();
+            IContainer container = builder.Build();
+            var server = container.Resolve<Server>();
+            server.Start(args);
+
         }
     }
 }
