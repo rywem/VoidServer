748cc285bc5f58cad31d629b8c5ea75ac3c13484
Ryan <cybervoid41@gmail.com>
2018-08-06 08:52:16 -0700

    added calculator operations

---
+++ VoidServerLibrary/Util/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidServerLibrary.Util
+{
+    public class Calculator
+    {
+    }
+
+    public class CalculatorRequest
+    {
+        object[] values;
+        
+    }
+}
---
+++ VoidServerLibrary/Util/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidServerLibrary.Util
+{
+    public enum Operation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+        PowerOf,
+        Postback
+    }
+}
